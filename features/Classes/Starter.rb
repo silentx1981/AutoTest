2f14959b9446fb68abac0a1a4695c78c4ad01d64
Raffael Wyss <rw@ivaris.ch>
2018-01-30 07:07:59 +0100

    Many Optimations

--- features/Classes/Starter.rb
+++ features/Classes/Starter.rb
@@ -7,10 +7,10 @@ class Starter
   def run(browser)
     $browser = LLH::Core::Browser.new(browser)
     @config = LLH::Core::Config.new
-    @config.setConfigByExcel('config/Init.xlsx')
+    @config.setConfigByExcel('config.Init')
 
     @excelProcess = LLH::Process::Excel.new(browser)
-    @excelProcess.run('ExcelProcess.xlsx')
+    @excelProcess.run('ExcelProcess')
 
   end
 
